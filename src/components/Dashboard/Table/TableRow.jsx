c197c62bdf2f0bcc0c296ce0a81fc162badc9632
sonu-kumar2001 <sonurocckkerr@gmail.com>
2021-10-08 19:04:55 +0530

    Added deals column and edited entry column

--- src/components/Dashboard/Table/TableRow.jsx
+++ src/components/Dashboard/Table/TableRow.jsx
@@ -11,17 +11,18 @@ export default function TableRow({ data }) {
     <tbody className="table-body">
       {data?.map((rowData, i) => (
         <tr className="table-row" key={i}>
-          <td className="td">{rowData?.Order}</td>
+          <td className="td">{ rowData?.Deal}</td>
           <td className="td">{rowData?.Login}</td>
           <td className="td no-wrap">{rowData?.Name}</td>
           <td className="td">{rowData?.PositionID}</td>
+          <td className="td">{rowData?.Order}</td>
           <td className="td no-wrap">
             {moment(rowData?.Time)
               .utcOffset("+05:30")
               .format("dddd, MMMM Do YYYY, h:mm:ss a")}
           </td>
-          <td className="td">{rowData?.Type}</td>
-          <td className="td">{rowData?.Entry}</td>
+          {/* <td className="td">{rowData?.Type}</td> */}
+          <td className="td">{rowData?.Entry === 1 ? "In" : "Out"}</td>
           <td className="td">{rowData?.Symbol}</td>
           <td className="td">{rowData?.Volume / 10000}</td>
           <td className="td">{rowData?.Price}</td>
